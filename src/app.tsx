0f845724cfe8dafc73565c82e6174036ea1409b6
Jordan Domovčijski <jordan.domovcijski@valatron.com>
2023-11-06 21:37:02 +0100

    fixed linting space in app

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,5 @@
 const App = () => (
-  <h1
-    className="text-3xl
-  
-  
-  font-bold  underline"
-  >
-    Hello world!
-  </h1>
+  <h1 className="text-3xl font-bold  underline">Hello world!</h1>
 );
 
 export default App;
