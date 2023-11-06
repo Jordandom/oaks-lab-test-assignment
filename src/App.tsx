4427069c99dd33bc4671f2904ff574a2e2ed64a1
Jordan Domovčijski <jordan.domovcijski@valatron.com>
2023-11-06 21:34:33 +0100

    test husky

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,12 @@
 const App = () => (
-  <h1 className="text-3xl font-bold  underline">Hello world!</h1>
+  <h1
+    className="text-3xl
+  
+  
+  font-bold  underline"
+  >
+    Hello world!
+  </h1>
 );
 
 export default App;
