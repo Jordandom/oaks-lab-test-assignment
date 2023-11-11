4e261d09a0b994d34f5feda10ca25fe12474e9ea
Jordan Domovčijski <jordan.domovcijski@valatron.com>
2023-11-11 16:02:27 +0100

    components, aliases, layout

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,9 @@
+import { Card } from '@components/card';
+
 const App = () => (
-  <h1 className="text-3xl font-bold  underline">Hello world!</h1>
+  <div className="flex h-screen flex-col items-center bg-white bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern.svg')] px-40 py-20 dark:bg-gray-900 dark:bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/hero-pattern-dark.svg')]">
+    <Card />
+  </div>
 );
 
 export default App;
---
+++ src/components/card.tsx
@@ -0,0 +1,9 @@
+import { Heading } from '@components/heading';
+
+export const Card = () => {
+  return (
+    <div className="max-w-sm rounded-lg border border-gray-200 bg-white p-6 shadow">
+      <Heading title="My startup progress" />
+    </div>
+  );
+};
